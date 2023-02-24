463e8b768f40e4554a03f7dd770cd9d1ee11bc7a
TasumLuke <114318995+TasumLuke@users.noreply.github.com>
2023-02-23 22:48:33 -0500

    Update code.asm

--- ServerSideTest/code.asm
+++ ServerSideTest/code.asm
@@ -1,1 +1,19 @@
-ssssssssssssss
+section .data
+    msg db 'Hello, world!', 0xa ; string to be printed
+    len equ $-msg ; calculate length of the string
+
+section .text
+    global _start ; declare the entry point of the program
+
+_start:
+    ; write the string to stdout
+    mov eax, 4 ; write system call
+    mov ebx, 1 ; stdout file descriptor
+    mov ecx, msg ; pointer to the string
+    mov edx, len ; length of the string
+    int 0x80 ; execute the system call
+
+    ; exit the program
+    mov eax, 1 ; exit system call
+    xor ebx, ebx ; exit code
+    int 0x80 ; execute the system call
